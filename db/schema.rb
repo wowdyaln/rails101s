e9cf60744fed3911dc93eedb2ea2cfb53a250be9
wowdyaln <rogersmk@gmail.com.tw>
2016-07-31 00:54:47 +0800

    39. rake db:migrate add_name_to_user
    refine users column :name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160730160924) do
+ActiveRecord::Schema.define(version: 20160730165210) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
@@ -40,9 +40,11 @@ ActiveRecord::Schema.define(version: 20160730160924) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["name"], name: "index_users_on_name", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
