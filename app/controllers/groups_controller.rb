660ffa972ae2d23c4e9650cd5e135343d1f0064f
wowdyaln <rogersmk@gmail.com.tw>
2016-07-30 18:29:58 +0800

    12. group/create 完成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,4 +7,22 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
+
+  def create
+    @group = Group.create(group_params)
+
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+
 end
