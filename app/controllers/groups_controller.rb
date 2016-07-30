26ef8624159487d8b48a8ee04695d16bca34cf29
wowdyaln <rogersmk@gmail.com.tw>
2016-07-30 18:46:41 +0800

    15. group/update 完成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -26,6 +26,17 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
+  def update
+    @group = Group.find(params[:id])
+
+    if @group.update(group_params)
+      redirect_to group_path(@group), notice: "修改討論版成功！"
+    else
+      render :edit
+    end
+  end
+
+
 
   private
 
