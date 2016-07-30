5d5af7a0674186af86e59bdcaebe0a7677e68faf
wowdyaln <rogersmk@gmail.com.tw>
2016-07-30 20:00:49 +0800

    22. 設定 posts 的 routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'groups#index'
 
-   resources :groups
+   resources :groups do
+     resources :posts, except: [:show, :index]
+   end
+   
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
