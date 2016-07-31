5e91433777970146f66d9446bafb93f361b9a057
wowdyaln <rogersmk@gmail.com.tw>
2016-07-31 23:51:01 +0800

    54. user 必須是 group 的 member 才能發表文章(post #new ; create) [gem] awesome_rails_console [比較] member_required 方法，sdlong使用 return if

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ActiveRecord::Base
          has_many :groups
          has_many :posts
 
+         has_many :group_users
+         has_many :participated_groups, through: :group_users, source: :group
+
+         def is_member_of?(group)
+           participated_groups.include?(group)
+         end
 end
