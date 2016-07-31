30f8f5fc87c8e522f16d20831ab508d47c1bc91e
wowdyaln <rogersmk@gmail.com.tw>
2016-07-31 17:33:02 +0800

    [refine]51. [小修改]調整 notice_message 位置 [邏輯]group/show 頁面，post.author 要跟current_user相同 才顯示 edit; delete 按鈕

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,10 @@
 class Post < ActiveRecord::Base
   validates :content, presence: true
   belongs_to :group
-  belongs_to :author, class_name: "User", foreign_key: :user_id 
+  belongs_to :author, class_name: "User", foreign_key: :user_id
+
+  def editable_by?(abc)
+    abc == author
+  end
+  
 end
