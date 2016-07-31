b2d16546eceee5d5b32913aca7e6fa97c7dbd272
wowdyaln <rogersmk@gmail.com.tw>
2016-07-31 17:12:15 +0800

    49. [Model] 建立 posts 跟 users 的關係

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   validates :content, presence: true
   belongs_to :group
+  belongs_to :author, class_name: "User", foreign_key: :user_id 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
          has_many :groups
+         has_many :posts
 
 end
