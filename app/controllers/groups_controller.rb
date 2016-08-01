04bab50ef52e3db8a88cd3e4d05f42040cda06c2
wowdyaln <rogersmk@gmail.com.tw>
2016-08-01 15:27:51 +0800

    56. groups_controller 增加 join; quit Action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -49,6 +49,33 @@ class GroupsController < ApplicationController
     end
   end
 
+  def join
+    @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "你已經成功加入此group了！"
+
+    else
+      flash[:notice] = "你已經是此 group成員了"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:notice] = "你已經退出此 group，後回有期~"
+    else
+      flash[:notice] = "你還不是成員，退無可退"
+    end
+
+    redirect_to group_path(@group)
+  end
+
   private
 
   def group_params
